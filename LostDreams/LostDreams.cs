ffe2338aa1bd37cb4ba140ac54414dec64c35532
Branden Wood <brandenek03912@hotmail.com>
2024-01-08 10:19:50 -0500

    init: add base project files

---
+++ LostDreams/LostDreams.cs
@@ -0,0 +1,14 @@
+﻿using ModdingAPI;
+
+namespace LostDreams
+{
+    public class LostDreams : Mod
+    {
+        public LostDreams() : base(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION) { }
+
+        protected override void Initialize()
+        {
+            Log($"{PluginInfo.PLUGIN_NAME} has been initialized");
+        }
+    }
+}
---
+++ LostDreams/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace LostDreams
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.4.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public static LostDreams LostDreams { get; private set; }
+
+        private void Start()
+        {
+            LostDreams = new LostDreams();
+        }
+    }
+}
