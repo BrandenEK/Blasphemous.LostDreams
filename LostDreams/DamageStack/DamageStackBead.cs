1282f91daef1ab28691239ea858e4139704564cb
Xanathar865 <152095463+Xanathar865@users.noreply.github.com>
2024-01-14 15:44:22 -0800

    Add DamageStackBead Class

---
+++ LostDreams/DamageStack/DamageStackBead.cs
@@ -0,0 +1,35 @@
+﻿using ModdingAPI.Items;
+using UnityEngine;
+
+namespace LostDreams.DamageStack;
+
+class DamageStackBead : ModRosaryBead
+{
+    protected override string Id => "RB502";
+
+    protected override string Name => Main.LostDreams.Localize("dsname");
+
+    protected override string Description => Main.LostDreams.Localize("dsdesc");
+
+    protected override string Lore => Main.LostDreams.Localize("dslore");
+
+    protected override bool CarryOnStart => false;
+
+    protected override bool PreserveInNGPlus => true;
+
+    protected override bool AddToPercentCompletion => true;
+
+    protected override bool AddInventorySlot => true;
+
+    protected override void LoadImages(out Sprite picture)
+    {
+        picture = Main.LostDreams.FileUtil.loadDataImages("damage-stack.png", new Vector2Int(30, 30), Vector2Int.zero, 32, 0, true, out Sprite[] images) ? images[0] : null;
+    }
+}
+
+class DamageStackEffect : ModItemEffectOnEquip
+{
+    protected override void ApplyEffect() => Main.LostDreams.EffectHandler.Activate("damage-stack");
+
+    protected override void RemoveEffect() => Main.LostDreams.EffectHandler.Deactivate("damage-stack");
+}
