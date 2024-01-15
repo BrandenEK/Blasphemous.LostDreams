fad58bea4f6a85a834ff74b6a2e55a9084787278
Xanathar865 <152095463+Xanathar865@users.noreply.github.com>
2024-01-14 16:17:16 -0800

    Add FlaskHealthBead class

---
+++ LostDreams/FlaskHealth/FlaskHealthBead.cs
@@ -0,0 +1,35 @@
+﻿using ModdingAPI.Items;
+using UnityEngine;
+
+namespace LostDreams.FlaskHealth;
+
+class FlaskHealthBead : ModRosaryBead
+{
+    protected override string Id => "RB504";
+
+    protected override string Name => Main.LostDreams.Localize("fhname");
+
+    protected override string Description => Main.LostDreams.Localize("fhdesc");
+
+    protected override string Lore => Main.LostDreams.Localize("fhlore");
+
+    protected override bool CarryOnStart => false;
+
+    protected override bool PreserveInNGPlus => true;
+
+    protected override bool AddToPercentCompletion => true;
+
+    protected override bool AddInventorySlot => true;
+
+    protected override void LoadImages(out Sprite picture)
+    {
+        picture = Main.LostDreams.FileUtil.loadDataImages("flask-health.png", new Vector2Int(30, 30), Vector2Int.zero, 32, 0, true, out Sprite[] images) ? images[0] : null;
+    }
+}
+
+class FlaskHealthEffect : ModItemEffectOnEquip
+{
+    protected override void ApplyEffect() => Main.LostDreams.EffectHandler.Activate("flask-health");
+
+    protected override void RemoveEffect() => Main.LostDreams.EffectHandler.Deactivate("flask-health");
+}
