d7b419a2b38009f533e988fdacc9c15821df9a01
unknown <brandenek03912@hotmail.com>
2024-01-21 19:25:12 -0500

    flask: use lorq at same time as taranto

---
+++ LostDreams/FlaskHealth/FlaskHealthPatches.cs
@@ -0,0 +1,20 @@
+﻿using Framework.Inventory;
+using Framework.Managers;
+using Gameplay.GameControllers.Penitent.Abilities;
+using HarmonyLib;
+
+namespace LostDreams.FlaskHealth;
+
+[HarmonyPatch(typeof(PrayerUse), "StartUsingPrayer")]
+class Prayer_Start_Patch
+{
+    public static void Postfix(Prayer prayer)
+    {
+        if (!Main.LostDreams.EffectHandler.IsActive("flask-health") || prayer == null || prayer.name != "PR09")
+            return;
+
+        Main.LostDreams.Log("RB504: Buffing Taranto prayer");
+        Core.InventoryManager.GetPrayer("PR07").Use();
+        //Core.InventoryManager.GetPrayer("PR08").Use();
+    }
+}
--- LostDreams/LostDreams.cs
+++ LostDreams/LostDreams.cs
@@ -2,6 +2,7 @@
 ﻿using LostDreams.ChargeTimeDecrease;
 ﻿using LostDreams.DamageRemovalOnce;
 using ModdingAPI;
+using LostDreams.FlaskHealth;
 
 namespace LostDreams;
 
@@ -17,6 +18,7 @@ public class LostDreams : Mod
         // Register all new items
         RegisterItem(new ChargeTimeBead().AddEffect<ChargeTimeEffect>()); // RB501
         RegisterItem(new DamageRemovalBead().AddEffect<DamageRemovalEffect>()); // RB503
+        RegisterItem(new FlaskHealthBead().AddEffect<FlaskHealthEffect>()); // RB504
 
         RegisterItem(new GuiltFragmentItem().AddEffect<GuiltFragmentEffect>()); // QI502
     }
