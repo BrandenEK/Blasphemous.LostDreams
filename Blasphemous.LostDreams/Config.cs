e1baeb449c647637de2a80ed53059f0e23385a92
unknown <brandenek03912@hotmail.com>
2024-02-21 19:15:29 -0500

    config: add documentation

--- Blasphemous.LostDreams/Config.cs
+++ Blasphemous.LostDreams/Config.cs
@@ -1,11 +1,18 @@
 ﻿
 namespace Blasphemous.LostDreams;
 
+/// <summary>
+/// Stores configuration settings
+/// </summary>
 public class Config
 {
+    /// <summary> The total number of charges you can go up to </summary>
     public int RB502_MAX_CHARGES = 20;
+    /// <summary> The multiplier added to damage when you have maximum charges </summary>
     public float RB502_MAX_MULTIPLIER = 2.0f;
 
+    /// <summary> The decimal multiplied by your maximum health to heal every tick </summary>
     public float HE501_REGEN_PERCENT = 0.015f;
+    /// <summary> How many seconds in between each heal tick </summary>
     public float HE501_REGEN_DELAY = 1f;
 }
