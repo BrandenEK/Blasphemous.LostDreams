47a21f13dce1c1168134dd5956cf65481e2c04f7
unknown <brandenek03912@hotmail.com>
2024-06-22 07:42:24 -0400

    npc: load full animation

---
+++ Blasphemous.LostDreams/Animation/AnimationInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Blasphemous.LostDreams.Animation;
+
+public class AnimationInfo
+{
+    public string Name { get; }
+    public Sprite[] Sprites { get; }
+    public float SecondsPerFrame { get; }
+
+    public AnimationInfo(string name, Sprite[] sprites, float secondsPerFrame)
+    {
+        Name = name;
+        Sprites = sprites;
+        SecondsPerFrame = secondsPerFrame;
+    }
+}
--- Blasphemous.LostDreams/Animation/AnimationLoader.cs
+++ Blasphemous.LostDreams/Animation/AnimationLoader.cs
@@ -1,9 +1,16 @@
 ﻿using Blasphemous.ModdingAPI.Files;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Blasphemous.LostDreams.Animation;
 
 public class AnimationLoader
 {
+    private readonly Dictionary<string, AnimationInfo> _animations = new();
+
+    public AnimationInfo this[string name] => _animations.TryGetValue(name, out var info)
+        ? info : throw new System.Exception($"Animation {name} was never loaded");
+
     public AnimationLoader(FileHandler file)
     {
         string infoPath = "animations.json";
@@ -13,9 +20,22 @@ public class AnimationLoader
             return;
         }
 
-        foreach (var importInfo in imports)
+        var options = new SpriteImportOptions()
         {
-            Main.LostDreams.LogWarning(importInfo.Name + " from " + importInfo.FilePath);
+            Pivot = new Vector2(0.5f, 0)
+        };
+
+        foreach (var import in imports)
+        {
+            if (!file.LoadDataAsFixedSpritesheet(import.FilePath, new Vector2(import.Width, import.Height), out Sprite[] spritesheet, options))
+            {
+                Main.LostDreams.LogError($"Failed to load {import.Name} from {import.FilePath}");
+                continue;
+            }
+
+            _animations.Add(import.Name, new AnimationInfo(import.Name, spritesheet, import.SecondsPerFrame));
         }
+
+        Main.LostDreams.Log($"Loaded {_animations.Count} animations");
     }
 }
