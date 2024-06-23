38676dc86bffc3d213570dd789aa32c8d717efc2
EltonZhang777 <280903158@qq.com>
2024-06-23 13:55:59 +0800

    adds `EventHandler`s for parrying
    `OnParryFail` triggers both when whiffing a parry and when attempting to parry a non-parriable attack.
    The 3 other handlers are self-explanatory.

--- Blasphemous.LostDreams/Events/EventHandler.cs
+++ Blasphemous.LostDreams/Events/EventHandler.cs
@@ -1,5 +1,7 @@
-﻿using Gameplay.GameControllers.Entities;
+﻿using Framework.Managers;
+using Gameplay.GameControllers.Entities;
 using Gameplay.GameControllers.Penitent;
+using Gameplay.GameControllers.Penitent.Abilities;
 
 namespace Blasphemous.LostDreams.Events;
 
@@ -10,6 +12,8 @@ internal class EventHandler
     public delegate void StandardEvent();
     public delegate void HitEvent(ref Hit hit);
     public delegate void EntityEvent(Entity entity);
+    public delegate void ParryEvent(Parry parry);
+    public delegate void GuardSlideEvent(GuardSlide guadeSlide);
 
     public event StandardEvent OnUsePrieDieu;
     public event StandardEvent OnExitGame;
@@ -19,6 +23,11 @@ internal class EventHandler
 
     public event StandardEvent OnPlayerKilled;
     public event StandardEvent OnEnemyKilled;
+
+    public event ParryEvent OnParryStart;
+    public event ParryEvent OnParryRiposte;
+    public event GuardSlideEvent OnParryGuardSlide;
+    public event ParryEvent OnParryFail;
 
     public void KillEntity(Entity entity)
     {
@@ -46,5 +55,26 @@ internal class EventHandler
     public void Reset()
     {
         OnExitGame?.Invoke();
+    }
+
+    public void ParryStart(Parry parry)
+    {
+        OnParryStart?.Invoke(parry);
+    }
+
+    public void ParryRiposte(Parry parry)
+    {
+        if (parry.SuccessParry) { OnParryRiposte?.Invoke(parry); }
+    }
+
+    public void ParryGuardSlide(GuardSlide guardSlide)
+    {
+        OnParryGuardSlide?.Invoke(guardSlide);
+    }
+
+    public void ParryFail(Parry parry)
+    {
+        if (!parry.SuccessParry && !Core.Logic.Penitent.GuardSlide.Casting)
+        { OnParryFail?.Invoke(parry); }
     }
 }
--- Blasphemous.LostDreams/Events/EventPatches.cs
+++ Blasphemous.LostDreams/Events/EventPatches.cs
@@ -1,5 +1,7 @@
-﻿using Gameplay.GameControllers.Enemies.Framework.Damage;
+﻿using Framework.Managers;
+using Gameplay.GameControllers.Enemies.Framework.Damage;
 using Gameplay.GameControllers.Entities;
+using Gameplay.GameControllers.Penitent.Abilities;
 using Gameplay.GameControllers.Penitent.Damage;
 using HarmonyLib;
 using Tools.Level.Interactables;
@@ -28,4 +30,37 @@ class Penitent_Damage_Patch
 class Enemy_Damage_Patch
 {
     public static void Prefix(ref Hit hit) => Main.LostDreams.EventHandler.DamageEnemy(ref hit);
+}
+
+[HarmonyPatch(typeof(Parry), "RaiseParryEvent")]
+class Penitent_Parry_Start_Patch
+{
+    public static void Prefix(Parry __instance) => Main.LostDreams.EventHandler.ParryStart(__instance);
+}
+
+[HarmonyPatch(typeof(Parry), "CheckParry")]
+class Penitent_Parry_Riposte_Patch
+{
+    public static void Postfix(Parry __instance)
+    {
+        Main.LostDreams.EventHandler.ParryRiposte(__instance);
+    }
+}
+
+[HarmonyPatch(typeof(GuardSlide), "CastSlide")]
+class Penitent_Parry_GuardSlide_Patch
+{
+    public static void Postfix(Hit hit, GuardSlide __instance)
+    {
+        Main.LostDreams.EventHandler.ParryGuardSlide(__instance);
+    }
+}
+
+[HarmonyPatch(typeof(Parry), "StopParry")]
+class Penitent_Parry_Fail_Patch
+{
+    public static void Prefix(Parry __instance)
+    {
+        Main.LostDreams.EventHandler.ParryFail(__instance);
+    }
 }
