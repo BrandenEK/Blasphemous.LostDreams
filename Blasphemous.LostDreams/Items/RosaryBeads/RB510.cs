8452a768ac5ad5ea759fa779a9bc5b3ffcafc932
unknown <brandenek03912@hotmail.com>
2024-06-24 21:42:02 -0400

    rb510: add placeholder icons and text

---
+++ Blasphemous.LostDreams/Items/RosaryBeads/RB510.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Blasphemous.LostDreams.Items.RosaryBeads;
+
+internal class RB510 : EffectOnEquip { }
+
+internal class RB511 : EffectOnEquip { }
+
+internal class RB512 : EffectOnEquip { }
+
+internal class RB513 : EffectOnEquip { }
+
+internal class RB514 : EffectOnEquip { }
--- Blasphemous.LostDreams/Items/RosaryBeads/RosaryBeadList.cs
+++ Blasphemous.LostDreams/Items/RosaryBeads/RosaryBeadList.cs
@@ -7,5 +7,11 @@ internal class RosaryBeadList(Config cfg) : ItemList<RosaryBead>
     public RosaryBead RB502 { get; } = new RosaryBead(new RB502(cfg.RB502));
     public RosaryBead RB503 { get; } = new RosaryBead(new RB503());
 
+    public RosaryBead RB510 { get; } = new RosaryBead(new RB510());
+    public RosaryBead RB511 { get; } = new RosaryBead(new RB511());
+    public RosaryBead RB512 { get; } = new RosaryBead(new RB512());
+    public RosaryBead RB513 { get; } = new RosaryBead(new RB513());
+    public RosaryBead RB514 { get; } = new RosaryBead(new RB514());
+
     public RosaryBead RB551 { get; } = new RosaryBead(new RB551());
 }
