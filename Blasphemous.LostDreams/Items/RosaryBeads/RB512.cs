69238bee5c3a78f48e500f11680a176ef7f98a11
EltonZhang777 <280903158@qq.com>
2024-06-27 12:14:24 +0800

    naci: RB512 misc. cleaning
    cleaned redundant code and logging.
    
    Smoothens the shockwave effect.
    
    Bug waiting to fix: it can attack enemies when any of the enemy's hitbox (awake detection hitbox, attack hitbox, etc.) comes in contact with the bead attack hitbox.

--- Blasphemous.LostDreams/Items/RosaryBeads/RB512.cs
+++ Blasphemous.LostDreams/Items/RosaryBeads/RB512.cs
@@ -1,9 +1,5 @@
 ﻿using Framework.Managers;
 using Gameplay.GameControllers.Entities;
-using Gameplay.GameControllers.Entities.Weapon;
-using Gameplay.GameControllers.Penitent;
-using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,9 +7,16 @@ namespace Blasphemous.LostDreams.Items.RosaryBeads;
 
 internal class RB512 : EffectOnEquip
 {
-    private RB512Config _config;
+    private float _currentTimer;
+    float _explosionDelay = 0.5f;
+    float _shockwaveDuration = 0.5f;
+    private Vector3 _shockwavePosition;
+
+    private bool _isActive = false;
 
     private RB512ExplosionAttack _explosionAttack;
+    private RB512Config _config;
+
 
     public RB512(RB512Config config)
     {
@@ -25,7 +28,44 @@ internal class RB512 : EffectOnEquip
 
     private void CreateExplosion(ref bool cancel)
     {
-        _explosionAttack.StartAttack();
+        if (!IsEquipped)
+        { return; }
+
+        _currentTimer = 0f;
+        _isActive = true;
+        
+        Core.Logic.Penitent.Audio.PlayVerticalAttackLanding();
+
+    }
+
+    protected override void OnUpdate()
+    {
+        if (!_isActive)
+        { return; }
+
+        _currentTimer += Time.deltaTime;
+        if ( _currentTimer >= _explosionDelay 
+            && _currentTimer < _explosionDelay + Time.deltaTime )
+        { 
+            _explosionAttack.StartAttack();
+            _shockwavePosition = Core.Logic.Penitent.GetPosition();
+            Core.Logic.CameraManager.ShockwaveManager.Shockwave(
+                _shockwavePosition, 
+                _shockwaveDuration * 0.5f,
+                0.05f, 
+                _config.ATTACK_RADIUS * 0.1f);
+        }
+        else if (_currentTimer >= _explosionDelay + _shockwaveDuration * 0.5f
+            && _currentTimer < _explosionDelay + _shockwaveDuration * 0.5f + Time.deltaTime)
+        {
+            _isActive = false;
+            Core.Logic.CameraManager.ShockwaveManager.Shockwave(
+                _shockwavePosition,
+                _shockwaveDuration * 0.5f,
+                _config.ATTACK_RADIUS * 0.1f,
+                1.5f) ;
+        }
+        
     }
 
 }
@@ -49,6 +89,11 @@ public class RB512ExplosionAttack
 
     private RB512Config _config;
 
+    private List<IDamageable> _damageableEntities;
+
+    /// <summary>
+    /// Constructor for RB512's explosion attack
+    /// </summary>
     public RB512ExplosionAttack(RB512Config config)
     {
         _config = config;
@@ -61,32 +106,39 @@ public class RB512ExplosionAttack
         };
     }
 
+    /// <summary>
+    /// Initiate a RB512 explosion attack
+    /// </summary>
     public void StartAttack()
     {
         Vector3 centerPosition = Core.Logic.Penitent.transform.position;
-        List<IDamageable> damageableEntities = GetDamageableEntitiesWithinCircleArea(centerPosition, _config.ATTACK_RADIUS);
-        for (int i = 0; i < damageableEntities.Count; i++)
+        _damageableEntities = GetDamageableEntitiesWithinCircleArea(centerPosition, _config.ATTACK_RADIUS);
+        AttackDamageableEntities(_hit, _damageableEntities);
+        if (_damageableEntities.Count > 0)
         {
-            Main.LostDreams.Log($"RB512's #{i} entity attacked: {damageableEntities[i]}");
-        }
-        AttackDamageableEntities(_hit, damageableEntities);
-        if (damageableEntities.Count > 0)
-        {
-            Core.Logic.Penitent.SleepTimeByHit(_hit);
+            OnHit();
         }
     }
 
-    private void OnHit(Hit weaponHit)
+    /// <summary>
+    /// Effects that are triggered only when hitting at least 1 entity.
+    /// </summary>
+    private void OnHit()
     {
+        Core.Logic.ScreenFreeze.Freeze(0.05f, 0.05f);
         Core.Logic.CameraManager.ProCamera2DShake.Shake(
-            0.5f, Vector3.down * 0.5f, 12, 0.2f, 0.01f, default(Vector3), 0.01f, true);
+            0.35f, Vector3.down * 0.5f, 12, 0.2f, 0.01f, default(Vector3), 0.01f, true);
         
     }
-
+    /// <summary>
+    /// Return a list of enemies within a circular area
+    /// </summary>
+    /// <param name="centerPos">The center of the circle</param>
+    /// <param name="radius">The radius of the circle</param>
+    /// <returns></returns>
     public List<IDamageable> GetDamageableEntitiesWithinCircleArea(Vector3 centerPos, float radius)
     {
-        Collider2D[] allCollidersInRange = Physics2D.OverlapCircleAll(centerPos, radius) ;
-
+        Collider2D[] allCollidersInRange = Physics2D.OverlapCircleAll(centerPos, radius);
         GameObject[] allObjectsInRange = new GameObject[allCollidersInRange.Length];
         List<IDamageable> allDamageablesInRange = new();
 
@@ -114,21 +166,15 @@ public class RB512ExplosionAttack
             }
         }
 
-
-        for (int i = 0; i < allDamageablesInRange.Count; i++)
-        {
-            Main.LostDreams.Log($"#{i} damageable detected: {allDamageablesInRange[i]}");
-        }
-
         return allDamageablesInRange;
     }
 
     private void AttackDamageableEntities(Hit hit, List<IDamageable> targets)
     {
-        this.OnHit(hit);
         for (int i = 0; i < targets.Count; i++)
         {
             targets[i].Damage(hit);
         }
     }
 }
+
