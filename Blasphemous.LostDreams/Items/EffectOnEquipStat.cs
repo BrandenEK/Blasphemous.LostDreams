33be103fb91564c6c301ee2fabbb755fc1a6de81
unknown <brandenek03912@hotmail.com>
2024-07-14 12:39:21 -0400

    core: actually add EffectOnEquipStat

---
+++ Blasphemous.LostDreams/Items/EffectOnEquipStat.cs
@@ -0,0 +1,21 @@
+﻿using Framework.FrameworkCore.Attributes.Logic;
+using Framework.Managers;
+using Gameplay.GameControllers.Entities;
+
+namespace Blasphemous.LostDreams.Items;
+
+internal class EffectOnEquipStat(EntityStats.StatsTypes statType, float statValue) : EffectOnEquip
+{
+    private readonly EntityStats.StatsTypes _statType = statType;
+    private readonly RawBonus _statBonus = new(statValue);
+
+    protected override void OnEquip()
+    {
+        Core.Logic.Penitent.Stats.GetByType(_statType).AddRawBonus(_statBonus);
+    }
+
+    protected override void OnUnequip()
+    {
+        Core.Logic.Penitent.Stats.GetByType(_statType).RemoveRawBonus(_statBonus);
+    }
+}
