a39a5eaed1a44e68eed1a5e9d3daca64520d5892
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-07 20:57:04 -0400

    rb504: add code outline

---
+++ Blasphemous.LostDreams/Items/RosaryBeads/RB504.cs
@@ -0,0 +1,12 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.LostDreams.Items.RosaryBeads;
+
+internal class RB504 : EffectOnEquip
+{
+    protected override void OnEquip()
+    {
+        // Remove this
+        ModLog.Error("Equipping perpetva bead!");
+    }
+}
--- Blasphemous.LostDreams/Items/RosaryBeads/RosaryBeadList.cs
+++ Blasphemous.LostDreams/Items/RosaryBeads/RosaryBeadList.cs
@@ -6,6 +6,7 @@ internal class RosaryBeadList(Config cfg) : ItemList<RosaryBead>
     public RosaryBead RB501 { get; } = new RosaryBead(new RB501());
     public RosaryBead RB502 { get; } = new RosaryBead(new RB502(cfg.RB502));
     public RosaryBead RB503 { get; } = new RosaryBead(new RB503());
+    public RosaryBead RB504 { get; } = new RosaryBead(new RB504());
 
     public RosaryBead RB551 { get; } = new RosaryBead(new RB551());
 }
