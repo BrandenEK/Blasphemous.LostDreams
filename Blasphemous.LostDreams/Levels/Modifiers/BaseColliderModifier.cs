2ace11f01d4496b96398c541bba5fa2bab76768c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-02-07 20:21:28 -0500

    levels: remove color info from colliders

--- Blasphemous.LostDreams/Levels/Modifiers/BaseColliderModifier.cs
+++ Blasphemous.LostDreams/Levels/Modifiers/BaseColliderModifier.cs
@@ -1,9 +1,6 @@
 ﻿using Blasphemous.Framework.Levels.Modifiers;
 using Blasphemous.Framework.Levels;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Blasphemous.ModdingAPI;
 
 namespace Blasphemous.LostDreams.Levels.Modifiers;
 
@@ -40,77 +37,5 @@ internal abstract class BaseColliderModifier : IModifier
     {
         obj.name = $"Collider[{data.id}]";
         obj.layer = LayerMask.NameToLayer(_layer);
-    }
-
-    /// <summary>
-    /// Generate components of a colored mesh of the given color
-    /// </summary>
-    protected void GenerateColoredMesh(
-        Color color,
-        ref GameObject obj,
-        out Mesh mesh,
-        out MeshFilter meshFilter,
-        out MeshRenderer meshRenderer)
-    {
-        // Create a mesh and set up the mesh filter and renderer
-        meshFilter = obj.AddComponent<MeshFilter>();
-        meshRenderer = obj.AddComponent<MeshRenderer>();
-
-        // Create a mesh for the polygon
-        mesh = new Mesh();
-        meshFilter.mesh = mesh;
-
-        // Create a material and set a color
-        Material material = new Material(Shader.Find("Sprites/Default"));
-        material.color = color;
-
-        // Assign the material to the MeshRenderer
-        meshRenderer.material = material;
-    }
-
-    /// <summary>
-    /// Try to find a property from object JSON that contains color parameters, and output the corresponding color
-    /// </summary>
-    protected bool TryGetColorFromObjectData(ObjectData data, out Color color)
-    {
-        string colorString;
-        try
-        {
-            colorString = data.properties.First(x => x.ToLower().StartsWith("color:"));
-        }
-        catch
-        {
-            color = new Color(0, 0, 0, 0);
-            return false;
-        }
-
-        List<float> colorParams = new();
-        try
-        {
-            colorParams = colorString.Substring(6).Trim().Split(',').Select(float.Parse).ToList();
-        }
-        catch
-        {
-            ModLog.Warn("Invalid color parameters given!");
-            color = new Color(0, 0, 0, 0);
-            return false;
-        }
-
-        if (colorParams.Count == 3)
-        {
-            color = new Color(colorParams[0], colorParams[1], colorParams[2]);
-        }
-        else if (colorParams.Count == 4)
-        {
-            color = new Color(colorParams[0], colorParams[1], colorParams[2], colorParams[3]);
-        }
-        else
-        {
-            ModLog.Warn("Invalid color parameters given!");
-            color = new Color(0, 0, 0, 0);
-            return false;
-        }
-
-        return true;
     }
 }
--- Blasphemous.LostDreams/Levels/Modifiers/BoxColliderModifier.cs
+++ Blasphemous.LostDreams/Levels/Modifiers/BoxColliderModifier.cs
@@ -30,34 +30,5 @@ internal class BoxColliderModifier : BaseColliderModifier
         var collider = obj.AddComponent<BoxCollider2D>();
         collider.size = _size;
         collider.offset = _offset;
-
-        // if there's a color property, color the collider
-        if (TryGetColorFromObjectData(data, out Color color))
-        {
-            GenerateColoredMesh(color, ref obj, out Mesh mesh, out MeshFilter mf, out MeshRenderer mr);
-
-            // move the mesh to the same position and size of the collider
-            Vector3[] vertices =
-            [
-            new Vector3(-0.5f * _size.x, -0.5f * _size.y) + (Vector3)_offset, // bottom left
-            new Vector3(0.5f * _size.x, -0.5f * _size.y) + (Vector3)_offset,  // bottom right
-            new Vector3(0.5f * _size.x, 0.5f * _size.y) + (Vector3)_offset,   // top right
-            new Vector3(-0.5f * _size.x, 0.5f * _size.y) + (Vector3)_offset   // top left
-            ];
-
-            int[] triangles =
-                [
-                0, 2, 1,
-                0, 3, 2
-                ];
-
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-
-            mr.allowOcclusionWhenDynamic = true;
-            mr.receiveShadows = true;
-            mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-            mr.sortingOrder = -20000000;
-        }
     }
 }
